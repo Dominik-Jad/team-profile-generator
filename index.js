db9bd283cd5719592410073f3158fa7290f05c01
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-02-25 12:57:27 +0000

    added questions for the manager

--- index.js
+++ index.js
@@ -12,4 +12,52 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const teamMembers = [];
 
+// questions for the user to answer about the manager
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+        validate:  answer => {
+            if (answer === "") {
+                return "Please enter a name";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?",
+        validate:  answer => {
+            if (answer === "") {
+                return "Please enter an id";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+        validate:  answer => {
+            if (answer === "") {
+                return "Please enter an email";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        validate:  answer => {
+            if (answer === "") {
+                return "Please enter an office number";
+            }
+            return true;
+        }
+    }
+];
